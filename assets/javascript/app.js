e282cfac28aa61c31df79acf98ea52bce6645f04
Dennis Clemente <dennisclemente@users.noreply.github.com>
2017-08-19 22:00:06 -0700

    Fixed reset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,7 +41,7 @@ var unansweredTally = 0;
 $(document).ready(function() {
 
 function initialScreen() {
-	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Trivia Game</a></p>";
 	$(".mainArea").html(startScreen);
 }
 
